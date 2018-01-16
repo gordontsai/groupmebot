eb4efdcf618d6844b74143ec6c29f8fb85fcf9cd
Gordon Tsai <gtsai13@gmail.com>
2018-01-16 17:07:29 -0600

    organized inputs

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 Simple groupme bot to post a message every week. Using this in groupme to reserve rooms for dance practice every week.
 
+Make sure to change access token and other inputs.
+
 Scheduling is done through AWS Lambda.
 
 The only required package is GroupyAPI which utilizes Python >3.4.
--- reservation.py
+++ reservation.py
@@ -1,18 +1,21 @@
 from groupy.client import Client
 import datetime
 
+#Hardcoded inputs, these need to be changed to suit your needs
+access_token="0FpwvtwlWizeagQa5K4hEgLPEZqhLzDVETYJGs0Q"
+group_name='PJW Calling Email Method'
+msg="PJW Calling for this Sunday: "+dt_str+"\n1. Trophy Room\n2. Rec\n3. Greg"
+
 curr_date=datetime.datetime.now()
 next_sunday=curr_date+datetime.timedelta(days=7)
 dt_str=str(next_sunday.month)+'/'+str(next_sunday.day)
 
-client=Client.from_token("0FpwvtwlWizeagQa5K4hEgLPEZqhLzDVETYJGs0Q")
+client=Client.from_token(access_token)
 
 for group in client.groups.list():
   # print(group.name)
-  if group.name=='PJW Calling Email Method':
+  if group.name==group_name:
     grp=group
-
-msg="PJW Calling for this Sunday: "+dt_str+"\n1. Trophy Room\n2. Rec\n3. Greg"
 
 grp.post(text=msg)
 
